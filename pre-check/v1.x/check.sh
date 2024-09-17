362fdde68ef1c005c130368313baed2c8ababda0
Brian Monroe <bmonroe@ucsb.edu>
2024-09-17 09:17:47 -0700

    Add logging functions

--- pre-check/v1.x/check.sh
+++ pre-check/v1.x/check.sh
@@ -5,27 +5,23 @@ usage() {
  echo "Options:"
  echo " -h,	Display this help message"
  echo " -v,	Enable verbose mode"
- echo " -l,	Specify path to a log file."
+ echo " -o,	Specify path to a log file."
 }
 
 
-while getopts "hvac:k:" flag; do
+while getopts "hvl:k:" flag; do
  case $flag in
    h) # Handle the -h flag
    # Display script help information
    usage
    exit 0 
    ;;
+   l) # Handle the -l with an argument
+   log_file=$OPTARG
+   ;;
    v) # Handle the -v flag
    # Enable verbose mode
    verbose=true
-   ;;
-   c) # Handle the -c flag with an argument
-   clustername=$OPTARG
-   # Specify the human readable clustername in the slack message. 
-   ;;
-   k) # Handle the -k with an argument
-   kubeconfig=$OPTARG
    ;;
    \?)
    # Handle invalid options
@@ -43,12 +39,35 @@ record_fail()
 {
     check_failed=$((check_failed+1))
     failed_check_names=$((failed_check_names+"$1 "))
+    log_info "${1} Test: Failed"
+    echo -e "==============================\n\n"
 }
+
+#Log verbose messages, but don't echo them unless the -v flag is used.
+log_verbose()
+{
+    if [ $log_file ]; then
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] ${1}" >> "$log_file"
+    fi
+    if [ "$verbose" = true ]; then 
+        echo "${1}"
+    fi
+}
+
+#Log Info/Error Messages and always echo them. 
+log_info()
+{
+    if [ $log_file ]; then
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] ${1}" >> "$log_file"
+    fi
+    echo "${1}"
+}
+
 
 
 check_bundles()
 {
-    echo ">>> Check all bundles ready..."
+    log_info "Starting helm bundle status check... "
 
     # fleet bundles should be ready
     # except mcc-harvester, which has Modified status in 1.0.3
@@ -56,18 +75,21 @@ check_bundles()
     pending=$(echo "$bundles" | yq '.items | any_c(.metadata.name != "mcc-harvester" and .spec.helm != null and .status.summary.ready == 0)')
 
     if [ "$pending" = "false" ]; then
-      echo "All Helm bundles are ready."
+      log_verbose "All Helm bundles are ready."
+      log_info "Helm-Bundles Test: Pass"
+      echo -e "==============================\n"
       return
     fi
-
-    echo "There are non-ready Helm bundles:"
-    echo "$bundles" | yq '.items[] | select(.spec.helm != null and .status.summary.ready == 0) | .metadata.namespace + "/" + .metadata.name'
+    # These are failures so send them to the info log
+    log_info "There are non-ready Helm bundles:"
+    log_info "$(echo "$bundles" | yq '.items[] | select(.spec.helm != null and .status.summary.ready == 0) | .metadata.namespace + "/" + .metadata.name')"
+    echo -e "\n"
     record_fail "Helm-Bundles"
 }
 
 check_harvester_bundle()
 {
-    echo ">>> Check the Harvester bundle is ready..."
+    log_info "Starting Harvester bundle status check..."
 
     current_summary=$(mktemp)
     kubectl get bundles.fleet.cattle.io/mcc-harvester -n fleet-local -o yaml | yq '.status.summary' > $current_summary
@@ -79,18 +101,19 @@ ready: 1
 EOF
 
     if ! diff <(yq -P 'sort_keys(..)' $current_summary) <(yq -P 'sort_keys(..)' $expected_summary); then
-        echo "Harvester bundle is not ready!"
+        log_info "Harvester bundle is not ready!"
         record_fail "Harvester-Bundles"
         return
     fi
-
-    echo "The Harvester bundle is ready."
+    log_verbose "All Harvester bundles are ready."
+    log_info "Harvester-Bundles Test: Pass"
+    echo -e "==============================\n"
 }
 
 check_nodes()
 {
     local failed="false"
-    echo ">>> Check all nodes are ready..."
+    log_info "Starting Node-Status check... "
 
     # Use a file to store the node state becuase we can't set the global variable inside the piped scope
     # The file is removed in the piped scope if there are not-ready nodes.
@@ -101,16 +124,16 @@ check_nodes()
     unschedulable=$(echo "$nodes" | yq '.items | any_c(.spec.unschedulable == true)')
 
     if [ "$unschedulable" = "true" ]; then
-        echo "There are unschedulable nodes:"
-        echo "$nodes" | yq '.items[] | select(.spec.unschedulable == true)  | .metadata.name'
+        log_info "There are unschedulable nodes:"
+        log_info "$(echo "$nodes" | yq '.items[] | select(.spec.unschedulable == true)  | .metadata.name')"
         rm -f $node_ready_state
     fi
 
     # nodes should not be tainted
     tainted=$(echo "$nodes" | yq '.items | any_c(.spec.taints != null)')
     if [ "$tainted" = "true" ]; then
-        echo "There are tainted nodes:"
-        echo "$nodes" | yq '.items[] | select(.spec.taints != null)  | .metadata.name'
+        log_info "There are tainted nodes:"
+        log_info "$(echo "$nodes" | yq '.items[] | select(.spec.taints != null)  | .metadata.name')"
         rm -f $node_ready_state
     fi
 
@@ -118,55 +141,60 @@ check_nodes()
     echo "$nodes" | yq .items[].metadata.name |
         while read -r node_name; do
             node_ready=$(kubectl get nodes $node_name -o yaml | yq '.status.conditions | any_c(.type == "Ready" and .status == "True")')
-
             if [ "$node_ready" = "false" ]; then
-                echo "Node $node_name is not ready!"
+                log_info "Node $node_name is not ready!"
                 rm -f $node_ready_state
             fi
         done
 
     if [ -e $node_ready_state ]; then
-        echo "All nodes are ready."
+        log_verbose "All nodes are ready."
+        log_info "Node-Status Test: Pass"
+        echo -e "==============================\n"
         rm $node_ready_state
     else
-        echo "There are non-ready nodes."
+        log_verbose "There are non-ready nodes."
         record_fail "Node-Status"
     fi
 }
 
 check_cluster()
 {
-    echo ">>> Check the CAPI cluster is provisioned..."
+    log_info "Starting CAPI Cluster State check..."
     cluster_phase=$(kubectl get clusters.cluster.x-k8s.io/local -n fleet-local -o yaml | yq '.status.phase')
 
     # cluster should be in provisioned
     if [ "$cluster_phase" != "Provisioned" ]; then
-        echo "Cluster is not provisioned ($cluster_phase)"
+        log_info "Cluster is not provisioned ($cluster_phase)"
         record_fail "CAPI-Cluster-State"
         return
     fi
 
-    echo "The CAPI cluster is provisioned."
+    log_verbose "The CAPI cluster is provisioned."
+    log_info "CAPI-Cluster-State Test: Pass"
+    echo -e "==============================\n"
 }
 
 check_machines()
 {
     local failed="false"
-    echo ">>> Check the CAPI machines count..."
+    log_info "Starting CAPI Machine Count check..."
 
     # machine count should be equal to node counts
     machine_count=$(kubectl get machines.cluster.x-k8s.io -n fleet-local -o yaml | yq '.items | length')
     node_count=$(kubectl get nodes -o yaml | yq '.items | length')
     if [ $machine_count -ne $node_count ]; then
-        echo "CAPI machine count is not equal to node count. There are orphan machines."
-        kubectl get nodes
-        kubectl get machines.cluster.x-k8s.io -n fleet-local
+        log_info "CAPI machine count (${machine_count}) is not equal to node count (${node_count}). There are orphan machines:"
+        log_info "$(kubectl get nodes)"
+        log_info "$(kubectl get machines.cluster.x-k8s.io -n fleet-local)"
         record_fail "CAPI-Machine-Count"
     else
-        echo "CAPI machine count is equal to node count."
+        log_verbose "CAPI machine count is equal to node count."
+        log_info "CAPI-Machine-Count Test: Pass"
+        echo -e "==============================\n"
     fi
 
-    echo ">>> Check the CAPI machines are running..."
+    log_info "Starting CAPI Machine State check..."
 
     # Use a file to store the machine state becuase we can't set the global variable inside the piped scope
     # The file is removed in the piped scope if there are not-ready machines.
@@ -178,28 +206,31 @@ check_machines()
             machine_phase=$(kubectl get machines.cluster.x-k8s.io/$machine_name -n fleet-local -o yaml | yq '.status.phase')
 
             if [ "$machine_phase" != "Running" ]; then
-                echo "CAPI machine $machine_name phase is not Running ($machine_phase)."
+                log_info "CAPI machine $machine_name phase is not Running but is: ($machine_phase)."
                 rm -f $machine_ready_state
             fi
         done
 
     if [ -e $machine_ready_state  ]; then
-        echo "The CAPI machines are provisioned."
+        log_verbose "The CAPI machines are provisioned."
+        log_info "CAPI-Machine-State Test: Pass"
         rm $machine_ready_state
     else
-        echo "There are non-ready CAPI machines."
+        log_verbose "There are non-ready CAPI machines."
         record_fail "CAPI-Machine-State"
     fi
 }
 
 check_volumes()
 {
-    echo ">>> Check Longhorn volumes..."
+    log_info "Starting Longhorn Volume Health Status check..."
 
     node_count=$(kubectl get nodes -o yaml | yq '.items | length')
 
     if [ $node_count -eq 1 ]; then
-        echo "Skip checking for single node cluster."
+        log_info "Skip checking for single node cluster."
+        log_info "Longhorn-Volume-Health-Status Test: Skipped"
+        echo -e "==============================\n"
         return
     fi
 
@@ -211,29 +242,30 @@ check_volumes()
     kubectl get engines.longhorn.io -n longhorn-system -o json |
         jq -r '.items | map(select(.status.currentState == "running")) | map(.metadata.name + " " + .metadata.labels.longhornvolume) | .[]' | {
             while read -r lh_engine lh_volume; do
-                echo checking running engine "${lh_engine}..."
+                log_verbose "Checking running engine: ${lh_engine}"
 
                 if [ $node_count -gt 2 ];then
                     volume_json=$(kubectl get volumes.longhorn.io/$lh_volume -n longhorn-system -o json)
                     # single-replica volumes should be handled exclusively
                     volume_replicas=$(echo $volume_json | jq -r '.spec.numberOfReplicas')
                     if [ $volume_replicas -eq 1 ]; then
-                        echo "Volume $lh_volume is a single-replica volume. Please consider shutting down the corresponding workload or adjusting its replica count before upgrading."
+                        log_info "Volume ${lh_volume} is a single-replica volume. Please consider shutting down the corresponding workload or adjusting its replica count before upgrading."
                         rm -f $healthy_state
                     else
                         robustness=$(echo $volume_json | jq -r '.status.robustness')
                         if [ "$robustness" = "healthy" ]; then
-                            echo "Volume $lh_volume is healthy."
+                            log_verbose "Volume ${lh_volume} is healthy."
                         else
-                            echo "Volume $lh_volume is degraded."
+                            log_info "Degraded Longhorn Volume found: ${lh_volume}"
                             rm -f $healthy_state
                         fi
                     fi
                 else
-                    # two node situation, make sure maximum two replicas are healthy
+                    # This is a two node situation since we skip this in single nodes and the previous section is 3 or more. 
+                    # Make sure maximum two replicas are healthy.
                     expected_replicas=2
 
-                    # replica 1 case
+                    # Replica 1 case
                     volume_replicas=$(kubectl get volumes.longhorn.io/$lh_volume -n longhorn-system -o jsonpath='{.spec.numberOfReplicas}')
                     if [ $volume_replicas -eq 1 ]; then
                         expected_replicas=1
@@ -242,9 +274,9 @@ check_volumes()
                     ready_replicas=$(kubectl get engines.longhorn.io/$lh_engine -n longhorn-system -o json |
                                     jq -r '.status.replicaModeMap | to_entries | map(select(.value == "RW")) | length')
                     if [ $ready_replicas -ge $expected_replicas ]; then
-                        echo "Volume $lh_volume is healthy."
+                        log_verbose "Volume ${lh_volume} is healthy."
                     else
-                        echo "Volume $lh_volume is degraded."
+                        log_info "Degraded Longhorn Volume found: ${lh_volume}"
                         rm -f $healthy_state
                     fi
                 fi
@@ -253,10 +285,12 @@ check_volumes()
         }
 
     if [ -e $healthy_state ]; then
-        echo "All volumes are healthy."
+        log_verbose "All volumes are healthy."
+        log_info "Longhorn-Volume-Health-Status Test: Pass"
+        echo -e "==============================\n"
         rm $healthy_state
     else
-        echo "There are volumes that need your attention!"
+        log_info "There are volumes that need your attention!"
         record_fail "Longhorn-Volume-Health-Status"
     fi
 }
@@ -264,7 +298,7 @@ check_volumes()
 # https://github.com/harvester/harvester/issues/3648
 check_attached_volumes()
 {
-    echo ">>> Check stale Longhorn volumes..."
+    log_info "Starting Stale Longhorn Volumes check..."
 
     # Use a file to store the clean state becuase we can't set the global variable inside the piped scope
     # The file is removed in the piped scope if any volume is stale.
@@ -274,12 +308,12 @@ check_attached_volumes()
     # for each attached volume
     echo "$volumes" | yq '.items[] | select(.status.state == "attached") | .metadata.namespace + " " + .metadata.name' | {
         while read -r vol_namespace vol_name; do
-            echo "Checking volume $vol_namespace/$vol_name..."
+            log_verbose "Checking volume: ${vol_namespace}/${vol_name}"
 
             # check .status.kubernetesStatus.workloadStatus is nil
             workloads=$(kubectl get volumes.longhorn.io/$vol_name -n $vol_namespace -o yaml | yq '.status.kubernetesStatus.workloadsStatus | length')
             if [ "$workloads" = "0" ]; then
-                echo "Volume $vol_namespace/$vol_name is attached but has no workload."
+                log_info "Volume $vol_namespace/$vol_name is attached but has no workload."
                 rm -f $clean_state
                 continue
             fi
@@ -292,7 +326,7 @@ check_attached_volumes()
             #     workloadType: VirtualMachineInstance
             is_stale=$(kubectl get volumes.longhorn.io/$vol_name -n $vol_namespace -o yaml | yq '.status.kubernetesStatus.workloadsStatus | any_c(.podStatus != "Running")')
             if [ "$is_stale" = "true" ]; then
-                echo "Volume $vol_namespace/$vol_name is attached but its workload is not running." 
+                log_info "Volume ${vol_namespace}/${vol_name} is attached but its workload is not running." 
                 rm -f $clean_state
             fi
             sleep 0.5
@@ -300,32 +334,36 @@ check_attached_volumes()
     }
 
     if [ -e $clean_state ]; then
-        echo "There is no stale Longhorn volume."
+        log_verbose "There is no stale Longhorn volume."
         rm $clean_state
+        log_info "Stale-Longhorn-Volumes Test: Pass"
+        echo -e "==============================\n"
     else
-        echo "There are stale volumes."
+        log_st "There are stale volumes."
         record_fail "Stale-Longhorn-Volumes"
     fi
 }
 
 check_error_pods()
 {
-    echo ">>> Check error pods..."
+    log_info "Starting Pod Status check..."
 
     pods=$(kubectl get pods -A -o yaml)
     no_ok=$(echo "$pods" | yq '.items | any_c(.status.phase != "Running" and .status.phase != "Succeeded")')
 
     if [ "$no_ok" = "false" ]; then
-        echo "All pods are OK."
+        logging_verbose "All pods are OK."
+        log_info "Pod-Status Test: Pass"
+        echo -e "==============================\n"
         return
     fi
 
-    echo "There are non-ready pods:"
-    echo "$pods" | yq '.items[] | select(.status.phase != "Running" and .status.phase != "Succeeded") | .metadata.namespace + "/" + .metadata.name'
+    log_info "There are non-ready pods:"
+    log_info "$(echo "$pods" | yq '.items[] | select(.status.phase != "Running" and .status.phase != "Succeeded") | .metadata.namespace + "/" + .metadata.name')"
     record_fail "Pod-Status"
 }
 
-# only works in Harvester nodes
+# Only works in Harvester nodes.
 check_free_space()
 {
     prom_ip=$(kubectl get services/rancher-monitoring-prometheus -n cattle-monitoring-system -o yaml | yq -e '.spec.clusterIP')
@@ -334,14 +372,43 @@ check_free_space()
     length=$(echo "$result" | jq 'length')
 
     if [ "$length" == "0" ]; then
-        echo "All nodes have more than 30GB free space."
+        log_verbose "All nodes have more than 30GB free space."
+        log_info "Node-Free-Space Test: Pass"
+        echo -e "==============================\n"
         return
     fi
 
-    echo "Nodes doesn't have enough free space:"
-    echo "$result" | jq -r '.[].metric.instance'
+    log_info "Nodes doesn't have enough free space:"
+    log_info "$(echo "$result" | jq -r '.[].metric.instance')"
     record_fail "Node-Free-Space"
 }
+
+#If a log file exists ask users if they want to clear it, or exit. 
+check_log_file()
+{
+    if [ $log_file ]; then
+        read -r -p "The file ${log_file} exists. Are you sure that you want to overwrite/clear the contents of that file? [Y/N] " response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+            echo '' > $log_file
+        else
+            echo -e "Choose a different filename for the log flag (-l) or move/rename the file and run the script again. \nExiting."
+            exit 1
+        fi
+    fi
+}
+
+check_log_file
+
+log_verbose "Script has started"
+
+log_info "This script is intended to be run from one of the Harvester cluster's Control Plane nodes."
+log_info "You should stop the script (Ctrl+C) and run it from there if this is not the case."
+# Pause for humans.
+sleep 2
+
+log_verbose "The OS release is: $(awk -F= '$1=="PRETTY_NAME" { print $2 ;}' /etc/os-release)"
+
+exit 0
 
 check_bundles
 check_harvester_bundle
@@ -354,10 +421,8 @@ check_error_pods
 check_free_space
 
 if [ $check_failed -gt 0 ]; then
-    echo ""
-    echo "There are $check_failed failing checks!"
+    log_info "WARN: There are $check_failed failing checks: ${failed_check_names}"
     exit 1
 else
-    echo ""
-    echo "All checks pass."
+    log_info "All checks have pass."
 fi
