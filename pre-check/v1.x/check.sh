dad75a1a3b0ae1fea8ed1275856aaf780fb0d7b6
Brian Monroe <bmonroe@ucsb.edu>
2024-10-25 07:20:26 -0700

    Take some things literally.

--- pre-check/v1.x/check.sh
+++ pre-check/v1.x/check.sh
@@ -375,7 +375,8 @@ check_free_space()
 {
     log_info "Starting Node Free Space check..."
     prom_ip=$(kubectl get services/rancher-monitoring-prometheus -n cattle-monitoring-system -o yaml | yq '.spec.clusterIP')
-    if [ -z "${prom_ip}" ]; then
+    # Skip the test if the variable is empty or literal "null"
+    if [[ -z "$ip_prom" ||  "$ip_prom" == "null" ]]; then
         log_info "WARN: The script wasn't able to find a valid install of the rancher-monitoring addon, so it could not validate that there is sufficent freespace to complete the upgrade\nTo verify this test manually, log into each of the nodes and run 'df -h /usr/local' to ensure there's more than 30 GB of free space available."
         log_info "Node-Free-Space Test: SKIPPED"
         echo -e "\n==============================\n"
